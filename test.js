d8a75772b268d3c5465e1be81b6ec48ad5408b4c
Hukilow <138042141+Hukilow@users.noreply.github.com>
2024-04-30 08:31:54 +0000

    Result

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 userScores = {
   "Aventurier.ère": 0,
-  "Jardinier.ière_en_herbe": 0,
+  "Jardinier.ère_en_herbe": 0,
   "Admirateur.rice_de_la_faune": 0,
   "Méditateur.rice": 0,
   "Artiste_passioné.e": 0,
@@ -20,6 +20,84 @@ function updateDosage(slider, questionElement) {
   } else {
     console.error("Output element not found");
   }
+}
+
+function calculateUserProfile() {
+  var maxScore = 0;
+  var topProfiles = [];
+
+  // Copiez les noms des profils et leurs scores dans un tableau
+  var profilesArray = Object.entries(userScores);
+
+  // Triez le tableau des profils par score décroissant
+  profilesArray.sort((a, b) => b[1] - a[1]);
+
+  // Obtenez le score maximum
+  maxScore = profilesArray[0][1];
+
+  // Obtenez tous les profils ayant le score maximum
+  topProfiles = profilesArray
+    .filter((profile) => profile[1] === maxScore)
+    .map((profile) => profile[0]);
+
+  // Remplacez les tirets bas par des espaces et formatez les profils
+  var formattedProfiles = topProfiles.map((profile) =>
+    profile.replace(/_/g, " "),
+  );
+
+  // Affichez les profils avec le score le plus élevé dans l'élément HTML
+  var profilInfoElement = document.getElementById("resultat");
+  var profileList = document.getElementById("profileList");
+  var profileImage = document.getElementById("profileImage");
+
+  if (formattedProfiles.length === 1) {
+    profilInfoElement.textContent =
+      "Vous êtes plutôt : " + formattedProfiles[0];
+  } else {
+    profilInfoElement.textContent = "Vous avez plusieurs profils possibles : ";
+  }
+
+  // Supprimer les éléments existants de la liste et de l'image
+  //profileList.innerHTML = "";
+  //profileImage.innerHTML = "";
+
+  // Définir le poids de la police maximum et minimum
+  const maxFontWeight = 900;
+  const minFontWeight = 400;
+
+  // Définir la taille de la police pour les trois premiers éléments
+  const largerFontSize = "24px";
+
+  // Triez les profils par score décroissant
+  const sortedProfiles = Object.entries(userScores).sort((a, b) => b[1] - a[1]);
+
+  // Créer et ajouter les éléments li pour chaque profil avec un poids de police dégradé
+  sortedProfiles.forEach(([profile, score], index) => {
+    const listItem = document.createElement("li");
+    // Calculer le poids de la police en fonction de la position du profil dans la liste triée
+    const fontWeight =
+      maxFontWeight -
+      (index / (sortedProfiles.length - 1)) * (maxFontWeight - minFontWeight);
+    listItem.style.fontWeight = fontWeight;
+
+    // Appliquer un style différent pour les trois premiers éléments
+    if (index < 3) {
+      listItem.style.fontWeight = "bold";
+      listItem.style.fontSize = largerFontSize;
+    }
+
+    listItem.textContent = `${score} : ${profile.replace(/_/g, " ")}`;
+    profileList.appendChild(listItem);
+  });
+
+  const img = document.getElementById("imgresult");
+  console.log(topProfiles[0]);
+  img.src = `./img/${topProfiles[0]}.png`;
+  const a = document.getElementById("hrefprofil");
+  a.href = `./profil/${topProfiles[0]}.html`;
+  const p = document.getElementById("hrefp");
+  p.innerText = `${topProfiles[0]}`;
+  return topProfiles;
 }
 
 function updateFlowerParts(questionElement, value) {
@@ -63,6 +141,9 @@ function selectButton(button, scores) {
 }
 
 function nextQuestion(currentQuestion, nextQuestion) {
+  if (nextQuestion === "result") {
+    calculateUserProfile();
+  }
   var currentQuestionDiv = document.querySelector(
     '.question[data-question="' + currentQuestion + '"]',
   );
@@ -89,13 +170,15 @@ function nextQuestion(currentQuestion, nextQuestion) {
     // Désactivez le bouton "Confirmer" à nouveau
     nextQuestionDiv.querySelector(".confirmer").disabled = true;
     nextQuestionDiv.querySelector(".confirmer").style.cursor = "not-allowed";
-  } else if (nextQuestion === "result") {
-    window.location.href = "resultat.html";
   }
 }
 
 function nextQuestionSlider(currentQuestion, nextQuestion, NumSlider) {
-  var output = parseInt(sliderValue);
+  if (isNaN(parseInt(sliderValue))) {
+    var output = 0;
+  } else {
+    var output = parseInt(sliderValue);
+  }
   var currentQuestionDiv = document.querySelector(
     '.question[data-question="' + currentQuestion + '"]',
   );
@@ -106,43 +189,257 @@ function nextQuestionSlider(currentQuestion, nextQuestion, NumSlider) {
     case 1:
       switch (output) {
         case 0:
-          userScores["Explorateur_aventureux"] += 1;
-          userScores["Explorateur_aventureux"] += 1;
+          userScores["Aventurier.ère"] += 0;
+          userScores["Jardinier.ère_en_herbe"] += 0;
+          userScores["Admirateur.rice_de_la_faune"] += 0;
+          userScores["Méditateur.rice"] += 0;
+          userScores["Artiste_passioné.e"] += 0;
+          userScores["Contemplatif.ve"] += 0;
+          userScores["Citadin.e_vert.e"] += 0;
+          userScores["Écologiste_engagé.e"] += 0;
+          userScores["Curieux.se_des_Sciences"] += 0;
+          userScores["Apprenti.e_explorateur.rice"] += 0;
           break;
         case 1:
-          userScores["Explorateur_aventureux"] += 1;
+          userScores["Aventurier.ère"] += 0;
+          userScores["Jardinier.ère_en_herbe"] += 0;
+          userScores["Admirateur.rice_de_la_faune"] += 0;
+          userScores["Méditateur.rice"] += 1;
+          userScores["Artiste_passioné.e"] += 1;
+          userScores["Contemplatif.ve"] += 1;
+          userScores["Citadin.e_vert.e"] += 0;
+          userScores["Écologiste_engagé.e"] += 0;
+          userScores["Curieux.se_des_Sciences"] += 0;
+          userScores["Apprenti.e_explorateur.rice"] += 2;
           break;
         case 2:
-          userScores["Explorateur_aventureux"] += 1;
+          userScores["Aventurier.ère"] += 1;
+          userScores["Jardinier.ère_en_herbe"] += 1;
+          userScores["Admirateur.rice_de_la_faune"] += 1;
+          userScores["Méditateur.rice"] += 2;
+          userScores["Artiste_passioné.e"] += 2;
+          userScores["Contemplatif.ve"] += 2;
+          userScores["Citadin.e_vert.e"] += 1;
+          userScores["Écologiste_engagé.e"] += 1;
+          userScores["Curieux.se_des_Sciences"] += 1;
+          userScores["Apprenti.e_explorateur.rice"] += 2;
+          break;
+        case 3:
+          userScores["Aventurier.ère"] += 2;
+          userScores["Jardinier.ère_en_herbe"] += 2;
+          userScores["Admirateur.rice_de_la_faune"] += 2;
+          userScores["Méditateur.rice"] += 2;
+          userScores["Artiste_passioné.e"] += 2;
+          userScores["Contemplatif.ve"] += 2;
+          userScores["Citadin.e_vert.e"] += 2;
+          userScores["Écologiste_engagé.e"] += 2;
+          userScores["Curieux.se_des_Sciences"] += 2;
+          userScores["Apprenti.e_explorateur.rice"] += 1;
+          break;
+        case 4:
+          userScores["Aventurier.ère"] += 3;
+          userScores["Jardinier.ère_en_herbe"] += 3;
+          userScores["Admirateur.rice_de_la_faune"] += 3;
+          userScores["Méditateur.rice"] += 3;
+          userScores["Artiste_passioné.e"] += 3;
+          userScores["Contemplatif.ve"] += 3;
+          userScores["Citadin.e_vert.e"] += 3;
+          userScores["Écologiste_engagé.e"] += 3;
+          userScores["Curieux.se_des_Sciences"] += 3;
+          userScores["Apprenti.e_explorateur.rice"] += 1;
           break;
       }
       break;
     case 2:
       switch (output) {
         case 0:
-          userScores["Explorateur_aventureux"] += 1;
-          userScores["Explorateur_aventureux"] += 1;
+          userScores["Aventurier.ère"] += 0;
+          userScores["Jardinier.ère_en_herbe"] += 0;
+          userScores["Admirateur.rice_de_la_faune"] += 0;
+          userScores["Méditateur.rice"] += 0;
+          userScores["Artiste_passioné.e"] += 0;
+          userScores["Contemplatif.ve"] += 0;
+          userScores["Citadin.e_vert.e"] += 0;
+          userScores["Écologiste_engagé.e"] += 0;
+          userScores["Curieux.se_des_Sciences"] += 0;
+          userScores["Apprenti.e_explorateur.rice"] += 2;
           break;
         case 1:
-          userScores["Jardinier_en_herbe"] += 10;
+          userScores["Aventurier.ère"] += 1;
+          userScores["Jardinier.ère_en_herbe"] += 1;
+          userScores["Admirateur.rice_de_la_faune"] += 0;
+          userScores["Méditateur.rice"] += 2;
+          userScores["Artiste_passioné.e"] += 2;
+          userScores["Contemplatif.ve"] += 2;
+          userScores["Citadin.e_vert.e"] += 2;
+          userScores["Écologiste_engagé.e"] += 0;
+          userScores["Curieux.se_des_Sciences"] += 0;
+          userScores["Apprenti.e_explorateur.rice"] += 3;
           break;
         case 2:
-          userScores["Explorateur_aventureux"] += 1;
+          userScores["Aventurier.ère"] += 1;
+          userScores["Jardinier.ère_en_herbe"] += 1;
+          userScores["Admirateur.rice_de_la_faune"] += 0;
+          userScores["Méditateur.rice"] += 2;
+          userScores["Artiste_passioné.e"] += 2;
+          userScores["Contemplatif.ve"] += 2;
+          userScores["Citadin.e_vert.e"] += 2;
+          userScores["Écologiste_engagé.e"] += 0;
+          userScores["Curieux.se_des_Sciences"] += 0;
+          userScores["Apprenti.e_explorateur.rice"] += 3;
+          break;
+        case 3:
+          userScores["Aventurier.ère"] += 2;
+          userScores["Jardinier.ère_en_herbe"] += 2;
+          userScores["Admirateur.rice_de_la_faune"] += 1;
+          userScores["Méditateur.rice"] += 3;
+          userScores["Artiste_passioné.e"] += 3;
+          userScores["Contemplatif.ve"] += 3;
+          userScores["Citadin.e_vert.e"] += 3;
+          userScores["Écologiste_engagé.e"] += 1;
+          userScores["Curieux.se_des_Sciences"] += 1;
+          userScores["Apprenti.e_explorateur.rice"] += 2;
+          break;
+        case 4:
+          userScores["Aventurier.ère"] += 3;
+          userScores["Jardinier.ère_en_herbe"] += 3;
+          userScores["Admirateur.rice_de_la_faune"] += 3;
+          userScores["Méditateur.rice"] += 2;
+          userScores["Artiste_passioné.e"] += 2;
+          userScores["Contemplatif.ve"] += 2;
+          userScores["Citadin.e_vert.e"] += 2;
+          userScores["Écologiste_engagé.e"] += 3;
+          userScores["Curieux.se_des_Sciences"] += 3;
+          userScores["Apprenti.e_explorateur.rice"] += 2;
           break;
       }
       break;
     case 3:
       switch (output) {
         case 0:
-          userScores["Explorateur_aventureux"] += 1;
-          userScores["Explorateur_aventureux"] += 1;
+          userScores["Aventurier.ère"] += 0;
+          userScores["Jardinier.ère_en_herbe"] += 0;
+          userScores["Admirateur.rice_de_la_faune"] += 0;
+          userScores["Méditateur.rice"] += 0;
+          userScores["Artiste_passioné.e"] += 0;
+          userScores["Contemplatif.ve"] += 0;
+          userScores["Citadin.e_vert.e"] += 0;
+          userScores["Écologiste_engagé.e"] += 0;
+          userScores["Curieux.se_des_Sciences"] += 0;
+          userScores["Apprenti.e_explorateur.rice"] += 0;
           break;
         case 1:
-          userScores["Explorateur_aventureux"] += 1;
+          userScores["Aventurier.ère"] += 1;
+          userScores["Jardinier.ère_en_herbe"] += 1;
+          userScores["Admirateur.rice_de_la_faune"] += 1;
+          userScores["Méditateur.rice"] += 1;
+          userScores["Artiste_passioné.e"] += 1;
+          userScores["Contemplatif.ve"] += 1;
+          userScores["Citadin.e_vert.e"] += 1;
+          userScores["Écologiste_engagé.e"] += 0;
+          userScores["Curieux.se_des_Sciences"] += 1;
+          userScores["Apprenti.e_explorateur.rice"] += 2;
           break;
         case 2:
-          userScores["Explorateur_aventureux"] += 1;
+          userScores["Aventurier.ère"] += 3;
+          userScores["Jardinier.ère_en_herbe"] += 3;
+          userScores["Admirateur.rice_de_la_faune"] += 3;
+          userScores["Méditateur.rice"] += 3;
+          userScores["Artiste_passioné.e"] += 3;
+          userScores["Contemplatif.ve"] += 3;
+          userScores["Citadin.e_vert.e"] += 3;
+          userScores["Écologiste_engagé.e"] += 0;
+          userScores["Curieux.se_des_Sciences"] += 3;
+          userScores["Apprenti.e_explorateur.rice"] += 2;
           break;
+        case 3:
+            userScores["Aventurier.ère"] += 3;
+            userScores["Jardinier.ère_en_herbe"] += 3;
+            userScores["Admirateur.rice_de_la_faune"] += 3;
+            userScores["Méditateur.rice"] += 3;
+            userScores["Artiste_passioné.e"] += 3;
+            userScores["Contemplatif.ve"] += 3;
+            userScores["Citadin.e_vert.e"] += 3;
+            userScores["Écologiste_engagé.e"] += 2;
+            userScores["Curieux.se_des_Sciences"] += 3;
+            userScores["Apprenti.e_explorateur.rice"] += 1;
+            break;
+        case 4:
+            userScores["Aventurier.ère"] += 2;
+            userScores["Jardinier.ère_en_herbe"] += 2;
+            userScores["Admirateur.rice_de_la_faune"] += 2;
+            userScores["Méditateur.rice"] += 2;
+            userScores["Artiste_passioné.e"] += 2;
+            userScores["Contemplatif.ve"] += 2;
+            userScores["Citadin.e_vert.e"] += 2;
+            userScores["Écologiste_engagé.e"] += 3;
+            userScores["Curieux.se_des_Sciences"] += 2;
+            userScores["Apprenti.e_explorateur.rice"] += 0;
+            break;
+      }
+      break;
+    case 4:
+      switch (output) {
+        case 0:
+          userScores["Aventurier.ère"] += 0;
+          userScores["Jardinier.ère_en_herbe"] += 0;
+          userScores["Admirateur.rice_de_la_faune"] += 0;
+          userScores["Méditateur.rice"] += 0;
+          userScores["Artiste_passioné.e"] += 0;
+          userScores["Contemplatif.ve"] += 0;
+          userScores["Citadin.e_vert.e"] += 0;
+          userScores["Écologiste_engagé.e"] += 0;
+          userScores["Curieux.se_des_Sciences"] += 0;
+          userScores["Apprenti.e_explorateur.rice"] += 0;
+          break;
+        case 1:
+          userScores["Aventurier.ère"] += 0;
+          userScores["Jardinier.ère_en_herbe"] += 0;
+          userScores["Admirateur.rice_de_la_faune"] += 0;
+          userScores["Méditateur.rice"] += 0;
+          userScores["Artiste_passioné.e"] += 0;
+          userScores["Contemplatif.ve"] += 0;
+          userScores["Citadin.e_vert.e"] += 0;
+          userScores["Écologiste_engagé.e"] += 0;
+          userScores["Curieux.se_des_Sciences"] += 0;
+          userScores["Apprenti.e_explorateur.rice"] += 0;
+          break;
+        case 2:
+          userScores["Aventurier.ère"] += 1;
+          userScores["Jardinier.ère_en_herbe"] += 1;
+          userScores["Admirateur.rice_de_la_faune"] += 0;
+          userScores["Méditateur.rice"] += 0;
+          userScores["Artiste_passioné.e"] += 0;
+          userScores["Contemplatif.ve"] += 0;
+          userScores["Citadin.e_vert.e"] += 1;
+          userScores["Écologiste_engagé.e"] += 0;
+          userScores["Curieux.se_des_Sciences"] += 0;
+          userScores["Apprenti.e_explorateur.rice"] += 2;
+          break;
+        case 3:
+            userScores["Aventurier.ère"] += 3;
+            userScores["Jardinier.ère_en_herbe"] += 3;
+            userScores["Admirateur.rice_de_la_faune"] += 2;
+            userScores["Méditateur.rice"] += 2;
+            userScores["Artiste_passioné.e"] += 2;
+            userScores["Contemplatif.ve"] += 2;
+            userScores["Citadin.e_vert.e"] += 3;
+            userScores["Écologiste_engagé.e"] += 2;
+            userScores["Curieux.se_des_Sciences"] += 2;
+            userScores["Apprenti.e_explorateur.rice"] += 1;
+            break;
+        case 4:
+            userScores["Aventurier.ère"] += 2;
+            userScores["Jardinier.ère_en_herbe"] += 2;
+            userScores["Admirateur.rice_de_la_faune"] += 3;
+            userScores["Méditateur.rice"] += 3;
+            userScores["Artiste_passioné.e"] += 3;
+            userScores["Contemplatif.ve"] += 3;
+            userScores["Citadin.e_vert.e"] += 2;
+            userScores["Écologiste_engagé.e"] += 3;
+            userScores["Curieux.se_des_Sciences"] += 3;
+            userScores["Apprenti.e_explorateur.rice"] += 1;
+            break;
       }
       break;
   }
